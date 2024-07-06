334d6ec754f20925881f1af137e7979acb84e8d6
Abdul Cassim Zaki <zakiabdul4@gmail.com>
2024-07-07 01:24:13 +0200

    First commit to GA using Python

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from ypstruct import structure
+
+# Cost function
+def sphere(x):
+    return sum(x**2)
+
+def costfunction(x):
+    return x
+
+# Problem Definition
+
+# GA Parameters
+
+# Run GA
+
+# Results
